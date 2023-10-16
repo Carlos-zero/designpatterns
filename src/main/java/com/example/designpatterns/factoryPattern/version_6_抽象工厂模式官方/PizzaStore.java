0333696b10e0553c953c478ebc5282c13cfa6111
Carlos-zero <44743130+Carlos-zero@users.noreply.github.com>
2023-10-16 14:30:41 +0000

    抽象工厂模式

---
+++ src/main/java/com/example/designpatterns/factoryPattern/version_6_抽象工厂模式官方/PizzaStore.java
@@ -0,0 +1,17 @@
+/**
+ * PizzaStore 还是保持工厂方法模式的样式，由工厂类提供披萨，自己进行后续处理
+ */
+public abstract class PizzaStore {
+ 
+	protected abstract Pizza createPizza(String item);
+ 
+	public Pizza orderPizza(String type) {
+		Pizza pizza = createPizza(type);
+		System.out.println("--- Making a " + pizza.getName() + " ---");
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+	}
+}
